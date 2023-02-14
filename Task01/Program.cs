15ef23db08ac17113cd5e61cb74eae0663a7a38b
Tamara <2010nfvfhf2010@gmail.com>
2023-02-14 19:26:15 +0300

    AddedTask01

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// ввести 2 числа
+// ответить является ли первое число квадратом второго
+
+Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a==b * b)
+{
+  Console.WriteLine($"{a}, квадрат {b}");
+}
+else
+{
+Console.WriteLine($"{a}, не квадрат {b}");
+}
+
+
